550a545c7b5869835f3e990bd326e7aa3fc8e1f5
devyboy <dev@udel.edu>
2020-01-15 13:12:33 -0500

    added delete all button, updated screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final Set<WordPair> _saved = Set<WordPair>();
+  Set<WordPair> _saved = Set<WordPair>();
   final List<WordPair> _suggestions = <WordPair>[];
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
   final ScrollController _scrollController = new ScrollController();
@@ -45,27 +45,76 @@ class RandomWordsState extends State<RandomWords> {
       ),
       body: _buildSuggestions(_scrollController),
       drawer: Drawer(
-        child: ListView(
+          child: Column(
         children: <Widget>[
-          Container(
-						height: 60.0,
-						child: DrawerHeader(
-            child: Text(
-							"Favorite Names",
-							style: TextStyle(fontSize: 20),
-							textAlign: TextAlign.center,
-						),
-            decoration: new BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                new BoxShadow(
-                  color: Colors.grey[300],
-                  offset: new Offset(0, 3.0),
-                )
-              ],
-            ),
-          )),
-          _buildFavorites(_scrollController),
+          FractionallySizedBox(
+              widthFactor: 1,
+              child: Container(
+                  height: 180.0,
+                  child: DrawerHeader(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Favorite Names",
+                          style: TextStyle(fontSize: 25.0),
+                          textAlign: TextAlign.center,
+                        ),
+                        Container(
+                            margin: EdgeInsets.only(top: 15.0),
+                            child: ButtonTheme(
+                              minWidth: 160.0,
+                              child: RaisedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _saved = Set<WordPair>();
+                                  });
+                                },
+                                color: Colors.red[400],
+                                child: Text(
+                                  "DELETE ALL",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                              ),
+                            ))
+                      ],
+                    ),
+                    decoration: new BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        new BoxShadow(
+                          color: Colors.grey[300],
+                          offset: new Offset(0, 3.0),
+                        )
+                      ],
+                    ),
+                  ))),
+          Expanded(
+              child: ListView.separated(
+                  controller: _scrollController,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  padding: const EdgeInsets.all(13.0),
+                  itemCount: _saved.length,
+                  separatorBuilder: (context, index) {
+                    return Divider();
+                  },
+                  itemBuilder: (BuildContext context, int index) {
+                    return (ListTile(
+                      title: Text(_saved.elementAt(index).asPascalCase,
+                          style: TextStyle(fontSize: 16.0)),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          setState(() {
+                            _saved.remove(_saved.elementAt(index));
+                          });
+                        },
+                      ),
+                    ));
+                  }))
         ],
       )),
       floatingActionButton: FloatingActionButton(
@@ -82,33 +131,9 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
-  Widget _buildFavorites(controller) {
-    return ListView.separated(
-				controller: controller,
-        shrinkWrap: true,
-        padding: const EdgeInsets.all(13.0),
-        itemCount: _saved.length,
-				separatorBuilder: (context, index) {
-					return Divider();
-				},
-        itemBuilder: (BuildContext context, int index) {
-          return (
-						ListTile(
-            title: Text(
-							_saved.elementAt(index).asPascalCase,
-							style: TextStyle(fontSize: 16.0)
-						),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                setState(() {
-                  _saved.remove(_saved.elementAt(index));
-                });
-              },
-            ),
-          ));
-        });
-  }
+  // Widget _buildFavorites(controller) {
+  //   return
+  // }
 
   Widget _buildSuggestions(controller) {
     return ListView.builder(
